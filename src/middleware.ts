c02d0903f4707e0df2306d9db67fab643326df76
Hayochan-man <taropado3.14@gmail.com>
2026-08-29 21:29:56 +0900

    AppHeaderにおいて、画像を出力できるようにmiddlewareを修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|register).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|register|.*\\..*).*)"],
 };
